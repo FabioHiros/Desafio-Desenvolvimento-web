ff9993ef4d189eacd83c228605de302ec2d23638
FabioHiros <fabio.matsumura@fatec.sp.gov.br>
2023-11-21 13:49:00 -0300

    33

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask,render_template, request , Response,jsonify,make_response,redirect,session,flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text,create_engine
+import json
+from crud import pessoadb,select_user
+import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
+app = Flask("__name__")
+app.secret_key = '_5#y2L"F4dasQ8dasdasc]/'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:159753@localhost:3306/bddesafio3'
+#variavel contendo o caminho para o banco de dados
+db_connection_string = 'mysql+mysqlconnector://root:159753@localhost:3306/bddesafio3'
+
+#conecta no banco de dados
+engine = create_engine(
+  db_connection_string)
+
+db=SQLAlchemy(app)
+
+class pessoas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True,nullable=False)
+    funcao = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.String(300),nullable=False)
+    nota = db.Column(db.Integer,nullable=False)
+
+    # def __init__(self,email,password,nome):
+    #     self.nome = nome
+    #     self.email = email
+    #     self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    
+    # def check_password(self,password):
+    #     return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/zale')
+def zale():
+    return render_template('zale.html')
+
+
+@app.route('/valere')
+def valere():
+    return render_template('valere.html')
+
+@app.route('/oracle')
+def oracle():
+    return render_template('oracle.html')
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+# @app.route('/usuario')
+# def usuario():
+#     user= pessoadb()
+
+#     return render_template('user.html', user=user)
+
+@app.route('/user',methods=['POST','GET'])
+def user():
+   
+    data= request.form
+    
+    if  cadastro(data) == False:
+        flash('EMAIL ALREADY EXISTS',category='error')
+        return render_template('registro.html')
+    else:
+        flash('Sucessfuly registered!',category='success')
+        return render_template('user.html', data=data)
+
+#seleciona usuário pelo id
+@app.route('/user/<id>')
+def select(id):
+    user=select_user(id)
+    return render_template('user.html',data=user)
+
+
+#Login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = pessoas.query.filter_by(email=email).first()
+        
+        if check_password_hash(user.password,password):
+            session['email'] = user.email
+            return render_template('/user.html',data=user)
+        else:
+            return render_template('login.html',error='Invalid user')
+
+    return render_template('login.html')
+
+def cadastro(data):
+  with engine.connect() as conn:
+    user= pessoas.query.filter_by(email=data['email']).first()
+    if user:
+      return False
+    else:
+      conn.execute(text(f"INSERT INTO pessoas( nome, email, funcao, password, nota) VALUES ( '{data['nome']}', '{data['email']}', '{data['funcao']}', '{generate_password_hash(data['password'],method='pbkdf2',salt_length=16)}','{data['nota']}')"))
+    
+      conn.commit()
---
+++ crud.py
@@ -0,0 +1,54 @@
+from sqlalchemy import create_engine, text
+from flask import Flask,flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# import os
+
+# db_connection_string = os.environ['DB_CONNECTION_STRING']
+# db_connection_string = 'mysql+mysqlconnector://root:159753@localhost/bddesafio3'
+
+app=Flask('__name__')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:159753@localhost:3306/bddesafio3'
+db = SQLAlchemy(app)
+
+
+#variavel contendo o caminho para o banco de dados
+db_connection_string = 'mysql+mysqlconnector://root:159753@localhost:3306/bddesafio3'
+
+#conecta no banco de dados
+engine = create_engine(
+  db_connection_string)
+
+#seleciona todas as pessoas
+def pessoadb():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from pessoas"))
+    pessoas = []
+    for row in result.all():
+      pessoas.append(row._asdict())
+    return pessoas
+  
+#seleciona uma pessoa
+def select_user(id):
+  a=pessoadb()
+  usuario= next((x for x in a if x['id']==int(id)),None) #função pra filtrar por id
+  return usuario
+
+# def teste(nome):
+#   with engine.connect() as conn:
+#     result = conn.execute(
+#       text("SELECT * FROM jobs WHERE nome = :val"),val= nome)
+#     rows = result.all()
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return dict(rows[0])
+    
+
+
+        
+                #  nome=data['nome'],
+                #  email=data['email'],
+                #  funcao=data['funcao'],
+                #  senha=data['senha'])
