c5be1d5d6ea0b9e4c6a1101f66287f6414e5c20c
fabio hiroshi <fabio.matsumura@fatec.sp.gov.br>
2024-05-20 19:41:09 -0300

    asdasd

--- config.sh
+++ config.sh
@@ -7,12 +7,14 @@ apt install curl
 apt install docker.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+sudo chown ubuntu:ubuntu /usr/local/bin/docker-compose
 
 
 git clone https://github.com/FabioHiros/Desafio-Desenvolvimento-web.git /home/ubuntu/app
 
 
 chown -R ubuntu:ubuntu /home/ubuntu/app
+
 
 
 cd /home/ubuntu/app
@@ -22,28 +24,24 @@ cd /home/ubuntu/app
 
 usermod -aG docker $USER
 
-
-cat <<EOT | sudo tee /etc/systemd/system/docker-compose-app.service
+bash -c 'cat <<EOF > /etc/systemd/system/docker-compose-app.service
 [Unit]
 Description=Docker Compose Application Service
 Requires=docker.service
 After=docker.service
 
 [Service]
-ExecStart=/usr/local/bin/docker-compose up -d
-ExecStop=/usr/bin/docker-compose down
-Restart=always
-RemainAfterExit=true
 WorkingDirectory=/home/ubuntu/app
+ExecStart=/usr/bin/docker compose up
+ExecStop=/usr/bin/docker compose down
+Restart=always
 User=ubuntu
-
 
 [Install]
 WantedBy=multi-user.target
-EOT
-
+EOF'
 
 systemctl daemon-reload
+systemctl enable docker-compose-app.service
 
-systemctl enable docker-compose-app
-systemctl start docker-compose-app
+systemctl start docker-compose-app.service
