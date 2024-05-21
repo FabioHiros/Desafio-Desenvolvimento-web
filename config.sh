71007910823491a3834c58a8fd5d6eecb0b5db5b
fabio hiroshi <fabio.matsumura@fatec.sp.gov.br>
2024-05-20 22:21:22 -0300

    meu deus

--- config.sh
+++ config.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-
+# Update and install necessary packages
 apt-get update -y
-mkdir /home/ubuntu/app
-apt install curl
-apt install docker.io -y
+apt-get install -y python3-venv curl docker.io
+
+# Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-sudo chown ubuntu:ubuntu /usr/local/bin/docker-compose
+chown ubuntu:ubuntu /usr/local/bin/docker-compose
 
-
+# Clone your repository
 git clone https://github.com/FabioHiros/Desafio-Desenvolvimento-web.git /home/ubuntu/app
 
-
+# Adjust permissions
 chown -R ubuntu:ubuntu /home/ubuntu/app
 
-
-
-cd /home/ubuntu/app
-
-/usr/local/bin/docker-compose up -d
-
-
-usermod -aG docker $USER
-
-bash -c 'cat <<EOF > /etc/systemd/system/docker-compose-app.service
+# Create and enable Docker Compose service
+cat <<EOF > /etc/systemd/system/docker-compose-app.service
 [Unit]
 Description=Docker Compose Application Service
 Requires=docker.service
@@ -32,16 +24,15 @@ After=docker.service
 
 [Service]
 WorkingDirectory=/home/ubuntu/app
-ExecStart=/usr/bin/docker compose up
-ExecStop=/usr/bin/docker compose down
+ExecStart=/usr/local/bin/docker-compose up -d
+ExecStop=/usr/local/bin/docker-compose down
 Restart=always
 User=ubuntu
 
 [Install]
 WantedBy=multi-user.target
-EOF'
+EOF
 
 systemctl daemon-reload
 systemctl enable docker-compose-app.service
-
 systemctl start docker-compose-app.service
